c450e5c5dcb65ee10a455cf3ca9bfc4b9a3d6038
Jonas Teuwen <j.teuwen@nki.nl>
2023-04-20 17:59:25 +0200

    Cleanup tiff utility

--- src/isyntax_to_tiff.c
+++ src/isyntax_to_tiff.c
@@ -17,7 +17,7 @@ void update_progress(int32_t total_progress, int32_t page_progress, int32_t page
 
 void write_page_to_tiff(TIFF *output_tiff, isyntax_t *isyntax, isyntax_cache_t *isyntax_cache, isyntax_level_t *level,
                         int32_t tile_width, int32_t tile_height, int32_t *total_tiles_written, int32_t total_tiles,
-                        clock_t global_start_time) {
+                        clock_t global_start_time, uint16_t compression_type, uint16_t quality) {
     int32_t width = libisyntax_level_get_width(level);
     int32_t height = libisyntax_level_get_height(level);
     int32_t scale = libisyntax_level_get_scale(level);
@@ -27,9 +27,13 @@ void write_page_to_tiff(TIFF *output_tiff, isyntax_t *isyntax, isyntax_cache_t *
     TIFFSetField(output_tiff, TIFFTAG_IMAGELENGTH, height);
     TIFFSetField(output_tiff, TIFFTAG_BITSPERSAMPLE, 8);
     TIFFSetField(output_tiff, TIFFTAG_SAMPLESPERPIXEL, 4);
-    TIFFSetField(output_tiff, TIFFTAG_COMPRESSION, COMPRESSION_JPEG);
-    // TODO: ImageDescription
-    TIFFSetField(output_tiff, TIFFTAG_JPEGQUALITY, 80);
+
+    if (compression_type == COMPRESSION_JPEG) {
+        TIFFSetField(output_tiff, TIFFTAG_COMPRESSION, COMPRESSION_JPEG);
+        TIFFSetField(output_tiff, TIFFTAG_JPEGQUALITY, quality);
+    } else if (compression_type == COMPRESSION_LZW) {
+        TIFFSetField(output_tiff, TIFFTAG_COMPRESSION, COMPRESSION_LZW);
+    }
 
     TIFFSetField(output_tiff, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
     TIFFSetField(output_tiff, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
@@ -53,7 +57,6 @@ void write_page_to_tiff(TIFF *output_tiff, isyntax_t *isyntax, isyntax_cache_t *
 
     int32_t tile_progress = 0;
     int32_t tiles_in_page = ((height - 1) / tile_height) * ((width - 1) / tile_width) + 2;
-    clock_t start_time = clock();
 
     for (int32_t y_coord = 0; y_coord < height; y_coord += tile_height) {
         for (int32_t x_coord = 0; x_coord < width; x_coord += tile_width) {
@@ -90,9 +93,6 @@ void write_page_to_tiff(TIFF *output_tiff, isyntax_t *isyntax, isyntax_cache_t *
             double elapsed_global_time = (double)(current_global_time - global_start_time) / CLOCKS_PER_SEC;
             double avg_time_per_tile = elapsed_global_time / (*total_tiles_written + tile_progress);
             double eta = avg_time_per_tile * (total_tiles - (*total_tiles_written + tile_progress));
-
-
-
             update_progress(total_progress, tile_percent, scale, eta);
 
             libisyntax_tile_free_pixels(pixels);
@@ -132,6 +132,9 @@ int main(int argc, char **argv) {
     int64_t cache_size = 2000;
     int32_t tile_size = 1024;
 
+    int compression_type = COMPRESSION_JPEG;
+    int quality = 80;
+
     for (int i = 3; i < argc; ++i) {
         if (strcmp(argv[i], "--tile-size") == 0) {
             if (i + 1 < argc) {
@@ -145,6 +148,38 @@ int main(int argc, char **argv) {
                 printf("Error: Missing value for --tile-size option.\n");
                 return -1;
             }
+        } else if (strcmp(argv[i], "--compression") == 0) {
+            if (i + 1 < argc) {
+                if (strcmp(argv[i + 1], "JPEG") == 0) {
+                    compression_type = COMPRESSION_JPEG;
+                } else if (strcmp(argv[i + 1], "LZW") == 0) {
+                    compression_type = COMPRESSION_LZW;
+                } else if (strcmp(argv[i + 1], "NONE") == 0) {
+                    compression_type = COMPRESSION_NONE;
+                } else {
+                    printf("Error: Invalid compression type. Supported types are JPEG, LZW, and NONE.\n");
+                    return -1;
+                }
+                i++; // Skip the next argument (compression type value)
+            } else {
+                printf("Error: Missing value for --compression option.\n");
+                return -1;
+            }
+        } else if (strcmp(argv[i], "--quality") == 0) {
+            if (i + 1 < argc) {
+                quality = atoi(argv[i + 1]);
+                if (quality < 0 || quality > 100) {
+                    printf("Error: Invalid quality value. Please provide an integer value between 0 and 100 for the quality.\n");
+                    return -1;
+                }
+                if (compression_type != COMPRESSION_JPEG) {
+                    printf("Warning: The --quality flag is ignored with the current compression type. Quality is only applicable to JPEG compressions.\n");
+                }
+                i++; // Skip the next argument (quality value)
+            } else {
+                printf("Error: Missing value for --quality option.\n");
+                return -1;
+            }
         } else if (strcmp(argv[i], "--cache-size") == 0) {
             if (i + 1 < argc) {
                 cache_size = parse_cache_size(argv[i + 1]);
@@ -156,6 +191,7 @@ int main(int argc, char **argv) {
                 printf("Error: Missing value for --cache-size option.\n");
                 return -1;
             }
+
         } else {
             printf("Error: Unknown option %s\n", argv[i]);
             return -1;
@@ -211,7 +247,7 @@ int main(int argc, char **argv) {
     for (int32_t level = start_at_page; level < num_levels; ++level) {
         isyntax_level_t *current_level = libisyntax_image_get_level(image, level);
         write_page_to_tiff(output_tiff, isyntax, isyntax_cache, current_level, tile_width, tile_height,
-                           &total_tiles_written, total_tiles, global_start_time);
+                           &total_tiles_written, total_tiles, global_start_time, compression_type, quality);
     }
 
     // Close the output TIFF file.
